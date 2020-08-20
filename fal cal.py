dc06edf4f946acf72179aa8e7f00dc76a21103a2
Lokesh Joshi <69955726+lokeshjoshi01@users.noreply.github.com>
2020-08-20 13:35:29 +0530

    Add files via upload

---
+++ fal cal.py
@@ -0,0 +1,36 @@
+a = float(input("enter value of a"))
+b = float(input("enter value of b"))
+print("1.addition")
+print("2.subtraction")
+print("3.multiplication")
+print("4.division")
+choice = int(input("enter your choise(1,2,3 or 4):"))
+if choice ==1:
+    if a==56 and b==9:
+        print("answer=77")
+    else:
+        result = a+b
+        print("answer=, result")
+elif choice ==2:
+    if a==45 and b==10:
+        print("answer=50")
+    else:
+        result = a-b
+        print("answer=",result)
+elif choice == 3:
+    if a==45 and b==3:
+        print("answer=555")
+    else:
+        result = a*b
+        print("answer+",result)
+elif choice ==4:
+    if a==56 and b==6:
+        print("answer=4")
+    else:
+        result=a/b
+        print("answer=",result)
+
+
+
+
+
